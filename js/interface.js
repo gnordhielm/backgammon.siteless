3135beac8bb4eca047ea8f2eb19cb473455f2b29
Gus Nordhielm <gnordhielm@gmail.com>
2017-02-07 19:53:18 -0800

    game can be played on one computer, obeying all rules

---
+++ js/interface.js
@@ -0,0 +1,16 @@
+// intro interface
+// add name, or be spectator, list who's in the game, 
+// is this game being used to end an argument, if you get there in the middle,
+// update the spectator
+
+
+// instructions button
+// nudge button
+// undo button
+// commit button
+// dice interface
+// forfeit button
+
+
+// end interface, play again or close up shop
+
